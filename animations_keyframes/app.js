02359c017fa2b1ddc08347f67ca8dce5fc89c301
Akili Inniss <rasheemkhan@rasheems-mbp.mynetworksettings.com>
2023-02-13 11:56:18 -0500

    classwork

---
+++ animations_keyframes/app.js
@@ -0,0 +1,24 @@
+
+const jump =  () => {
+    let mario = document.querySelector('.ball')
+    mario.classList.toggle('jump2')
+    
+       }
+
+
+
+const pulse =  () => {
+    let mario2 = document.querySelector('.ball2')
+    mario2.classList.toggle('pulse2')
+}
+
+
+const flash =  () => {
+    let mario3 = document.querySelector('.ball3')
+    mario3.classList.toggle('flash2')
+}
+
+const zoomIn =  () => {
+    let mario4 = document.querySelector('.ball4')
+    mario4.classList.toggle('zoomIn2')
+}
