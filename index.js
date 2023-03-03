be23a11f173640d30817e29007d37caff4f3866d
Keels <107443714+Keelsthegreat@users.noreply.github.com>
2023-03-03 01:02:29 +0000

    Added assignment

---
+++ index.js
@@ -0,0 +1,5 @@
+let j = 98
+for(let i=99; i > 0; i--)
+ {j=i-1
+    console.log(`${i},"Bottles of cream soda on the wall, ${i}, bottles of cream soda. Take one down and pass it around, ${j}, bottles of cream soda on the wall."`)
+}
